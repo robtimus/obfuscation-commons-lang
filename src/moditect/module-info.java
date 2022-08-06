6aed708f2c0a97effa433367e1811af245b09a3c
Rob Spoor <rob@icemanx.nl>
2022-08-06 14:59:04 +0200

    Use latest parent Added profile for testing as proper module Open the single package to commons-lang3 Added GitHub action

--- src/moditect/module-info.java
+++ src/moditect/module-info.java
@@ -3,4 +3,6 @@ module com.github.robtimus.obfuscation.commons.lang {
     requires transitive org.apache.commons.lang3;
 
     exports com.github.robtimus.obfuscation.commons.lang3;
+
+    opens com.github.robtimus.obfuscation.commons.lang3 to org.apache.commons.lang3;
 }
